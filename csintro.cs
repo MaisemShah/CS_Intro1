3a117b7da1267cae8a4aec909dc31e984e17e733
MaisemShah <54338906+MaisemShah@users.noreply.github.com>
2019-09-24 10:19:59 -0500

    Add files via upload

---
+++ csintro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CS_Intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lol = "huhhh " + "nooooo";
+            int so1 = 750;
+            int so9 = 798;
+            float so3 = 332;
+            double s04 = 231;
+            bool s009 = false;
+
+
+            Console.WriteLine(so1);
+            Console.WriteLine(so3);
+            Console.WriteLine(s04);
+            Console.WriteLine(so9);
+            Console.WriteLine(s009);
+            Console.WriteLine(lol);
+            Console.ReadKey();
+
+            string concat2 = lol + "" + so1;
+            Console.WriteLine(concat2);
+            Console.ReadKey();
+
+
+            float t2 = 5.5f + 10f;
+            Console.WriteLine(t2);
+
+            const bool oh = true;
+            const double siii = 9.8797;
+            const string op = "mahmem";
+
+            int fruitgang = so1 * so9;
+            Console.WriteLine(fruitgang);
+            Console.ReadKey();
+        }
+    }
+}
